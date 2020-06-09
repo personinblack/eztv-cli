7a7291c058d5fea8b9404723963286bdf77c9ca0
personinblack <berkay@tuta.io>
2020-06-09 03:31:36 +0300

    A working prototype

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+mod show;
+use show::{ Episode, Show };
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Opt::from_args();
+    let eps =
+        if args.all {
+            let mut all_eps: Vec<Episode> = vec!();
+            let mut page = 1;
+            loop {
+                if let Ok(ep) = &mut Show::new(args.show).eps(page, args.limit).await {
+                    all_eps.append(ep);
+                    page += 1;
+                } else {
+                    break;
+                }
+            }
+            all_eps
+        } else {
+            Show::new(args.show).eps(args.page, args.limit).await?
+        };
+
+    for ep in eps {
+        println!("{}", ep);
+        println!("\n");
+    }
+
+    Ok(())
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "eztv cli")]
+struct Opt {
+    #[structopt(short, long, help = "Show's id at IMDB")]
+    show: u32,
+    #[structopt(short, long, default_value = "1", help = "Page")]
+    page: u32,
+    #[structopt(short, long, default_value = "15", help = "Amount of torrents per page")]
+    limit: u32,
+    #[structopt(short, long, help = "Grabs all shows")]
+    all: bool
 }
---
+++ src/show.rs
@@ -0,0 +1,48 @@
+use std::fmt;
+use serde::{ Serialize, Deserialize };
+use reqwest::Url;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Episode {
+    filename: String,
+    magnet_url: String,
+    title: String,
+    season: String,
+    episode: String,
+    size_bytes: String
+}
+
+impl fmt::Display for Episode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "[0;93m->[0;94m {} :: {}MiB\n\n[0;1m{}",
+               self.title,
+               self.size_bytes.parse::<u32>().unwrap() / 1048576,
+               self.magnet_url
+              )
+    }
+}
+
+pub struct Show {
+    id: u32
+}
+
+impl Show {
+    pub fn new(id: u32) -> Show {
+        let show = Show { id };
+        show
+    }
+
+    pub async fn eps(&self, page: u32, limit: u32) -> Result<Vec<Episode>, Box<dyn std::error::Error>> {
+        let mut episodes: Vec<Episode> = vec!();
+
+        let show: serde_json::Value = serde_json::from_str(&reqwest::get(
+            Url::parse(
+                &format!("https://eztv.io/api/get-torrents?imdb_id={}&page={}&limit={}",
+                         self.id, page, limit)
+            ).unwrap()
+        ).await?.text().await?)?;
+        episodes.append(&mut serde_json::from_value::<Vec<Episode>>(show["torrents"].clone())?);
+
+        Ok(episodes)
+    }
+}
